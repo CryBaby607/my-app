0af4e7a4799b826aef9e97c99376e54f7f61ebc3
Alexx1257 <alejandromacias1257@gmail.com>
2025-11-06 14:32:43 -0600

    06/11/2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,41 @@
 import { Routes, Route } from 'react-router-dom'; 
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+// Páginas Públicas
 import Home from './pages/Home.jsx'
 import Categoria from './pages/Categoria.jsx'
 import DetalleProducto from './pages/DetalleProducto.jsx'
 import Carrito from './pages/Carrito.jsx'
 
+// Páginas de Admin
+import AdminLogin from './pages/admin/AdminLogin.jsx'
+
+
 function App() {
 
   return (
     <>
-      {/* Navbar */}
-      <Navbar />
+      {/* Navbar solo se muestra en rutas públicas */}
+      <Routes>
+        <Route path="/*" element={
+          <>
+            <Navbar />
+            <main className="main-content with-header">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/categoria/:category" element={<Categoria />} />
+                <Route path="/producto/:id" element={<DetalleProducto />} />
+                <Route path="/carrito" element={<Carrito />} />
+              </Routes>
+            </main>
+            <Footer />
+          </>
+        } />
+        <Route path="/admin/login" element={<AdminLogin />} />
 
-      <main className="main-content with-header"> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categoria/:category" element={<Categoria />} />
-          <Route path="/producto/:id" element={<DetalleProducto />} />
-          <Route path="/carrito" element={<Carrito />} />
-        </Routes>
-      </main>
-
-      <Footer />
+      </Routes>
     </>
   )
 }
