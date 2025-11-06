443491c6656368db957991227c77acb6b0ecc349
Alexx1257 <alejandromacias1257@gmail.com>
2025-11-05 18:35:39 -0600

    05/11/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-// App.jsx
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx' // 1. Importar el componente Navbar
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom'; 
+import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
 
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       {/* 2. Renderizar el componente Navbar */}
       <Navbar />
 
-      {/* Contenido principal de la aplicación */}
-        <main className={`main-content ${showLayout ? 'with-header' : ''}`}>
+      <main className="main-content with-header"> 
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' 
 
 // Importar estilos base en orden
 import './styles/base/reset.css'
@@ -12,6 +13,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
